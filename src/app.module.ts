49eee04c99e683f6e11c470e9e2cfeb37b06b608
alepaillas <alepaillas@gmail.com>
2025-05-19 02:32:45 -0400

    add api key auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 // src/app.module.ts
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThreeCxModule } from './three-cx/three-cx.module';
 import { OdooModule } from './odoo/odoo.module';
+import { ApiKeyMiddleware } from './auth/api-key.middleware';
 
 @Module({
   imports: [ThreeCxModule, OdooModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyMiddleware).forRoutes('*');
+  }
+}
---
+++ src/auth/api-key.middleware.ts
@@ -0,0 +1,23 @@
+// src/auth/api-key.middleware.ts
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ApiKeyMiddleware implements NestMiddleware {
+  constructor(private readonly configService: ConfigService) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const apiKey = req.headers['x-api-key'];
+    const validApiKey = this.configService.get<string>('API_KEY');
+
+    if (!apiKey || apiKey !== validApiKey) {
+      return res.status(401).json({
+        statusCode: 401,
+        message: 'Unauthorized - Invalid API Key',
+      });
+    }
+
+    next();
+  }
+}
